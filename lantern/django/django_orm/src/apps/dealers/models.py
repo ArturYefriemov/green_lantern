0fd5d287f61258e93531bbe8e7e61a63226176bd
ArturYefriemov <arthurq@ukr.net>
2020-06-16 00:05:18 +0300

    django orm

--- lantern/django/django_orm/src/apps/dealers/models.py
+++ lantern/django/django_orm/src/apps/dealers/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Index
 
 
 class Country(models.Model):
-    country_id = models.AutoField(primary_key=True)
-    country_name = models.CharField(max_length=30)
-    country_number = models.CharField(max_length=4, unique=True)
+    country_name = models.CharField(max_length=30, blank=True, null=False)
+    country_number = models.CharField(max_length=4, blank=True, null=False)
 
     def __str__(self):
         return self.country_name
@@ -17,7 +17,6 @@ class Country(models.Model):
 
 
 class City(models.Model):
-    city_id = models.AutoField(primary_key=True)
     city_name = models.CharField(max_length=30)
     country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
 
@@ -29,8 +28,7 @@ class City(models.Model):
         verbose_name_plural = _('Cities')
 
 
-class Dealer(models.Model):
-    dealer_id = models.AutoField(primary_key=True)
+class Dealer(User):
     dealer_name = models.CharField(max_length=30)
     dealer_email = models.CharField(max_length=60)
     city_id = models.ForeignKey(City, on_delete=models.CASCADE)
